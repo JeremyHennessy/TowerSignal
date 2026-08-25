d51535e59490ed679dda9c98f463d3df9d69df57
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-08-25 12:11:51 -0300

    Build 014: load workflow presentation styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,6 @@ import './styles/nys.css'
 import './styles/redesign.css'
 import './styles/redesign-fixes.css'
 import './styles/acris.css'
+import './styles/workflow.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
