26e64d687441f4ddc1005801b88ec026369db3f0
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-09-24 13:48:29 -0300

    Use rolled-up family contacts in sales CRM

--- src/components/AdminCompanyProfilePage.tsx
+++ src/components/AdminCompanyProfilePage.tsx
@@ -158,7 +158,7 @@ export function AdminCompanyProfilePage({ companyId }: { companyId: string }) {
       </section>
     </div>
 
-    <CompanySalesCrmPanel companyId={model.masterId} companyName={model.master.rollup_name || model.master.legal_name || model.master.canonical_name} />
+    <CompanySalesCrmPanel companyId={model.masterId} companyIds={model.members.map(member => member.company_id)} companyName={model.master.rollup_name || model.master.legal_name || model.master.canonical_name} />
     <CompanyAdminPanel companyId={model.selected.company_id} canonicalName={model.selected.canonical_name} />
     <CompanyFamilyPanel companyId={model.selected.company_id} />
     <CompanyAuditHistory companyId={model.selected.company_id} />
